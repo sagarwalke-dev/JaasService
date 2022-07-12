e39c889549c8fe8fc96493216c5c96ed9c16904e
Sagar Walke <sagarwalke7030@gmail.com>
2022-07-12 23:02:11 +0530

    initial commit

---
+++ src/main/java/com/jaas/JaasServiceApplication.java
@@ -0,0 +1,25 @@
+package com.jaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Sagar Walke
+ * @email sagarwalke7030@gmail.com
+ * @date 12-Jul-2022
+ */
+@SpringBootApplication
+@Slf4j
+public class JaasServiceApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(JaasServiceApplication.class, args);
+		log.info("JassServiceApplication started successfully.");
+	}
+
+}
---
+++ src/test/java/com/jaas/JaasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
